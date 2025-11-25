8b01e6bd0cab9f45c197f4898814fd9b6d73e814
muruganantham <muruganantham@wbcsoftwarelab.com>
2025-11-25 11:29:13 +0530

    before angular 18 update

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,8 +1,8 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 import { provideZoneChangeDetection } from '@angular/core';
 
-const bootstrap = () => bootstrapApplication(AppComponent, { providers: [ provideZoneChangeDetection(), ...(config.providers || []) ] });
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, { providers: [ provideZoneChangeDetection(), ...(config.providers || []) ] }, context);
 
 export default bootstrap;
