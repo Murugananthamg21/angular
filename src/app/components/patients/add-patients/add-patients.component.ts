99149647ab70577550783f323e3cc75d8b33b677
muruganantham <muruganantham@wbcsoftwarelab.com>
2025-11-26 17:46:04 +0530

    local commit

--- src/app/components/patients/add-patients/add-patients.component.ts
+++ src/app/components/patients/add-patients/add-patients.component.ts
@@ -1,11 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { fadeIn, slideToggle } from '../../../core/animations/animations';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-patients',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-patients.component.html',
   styleUrl: './add-patients.component.css',
   animations: [fadeIn, slideToggle]
@@ -13,4 +14,13 @@ import { CommonModule } from '@angular/common';
 export class AddPatientsComponent {
   isShow = signal(false);
   toggle() { this.isShow.update(a => !a) }
+
+
+  nameControl = new FormControl<string | null>('', Validators.required);
+
+  constructor() {
+    this.nameControl.valueChanges.subscribe(value => {
+      console.log("🔥 valueChanges fired:", value);
+    });
+  }
 }
