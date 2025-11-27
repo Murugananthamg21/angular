c9f882d03ff0ef4c54b812be69992fb6dcc17590
muruganantham <muruganantham@wbcsoftwarelab.com>
2025-11-27 18:22:53 +0530

    local commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
 import { MatIconModule } from '@angular/material/icon';
-import { AuthComponent } from './components/auth/auth.component';
 import { LoadingBarModule } from '@ngx-loading-bar/core';
 import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 
@@ -10,7 +8,7 @@ import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LayoutComponent, MatIconModule, AuthComponent, LoadingBarModule, LoadingBarHttpClientModule],
+  imports: [RouterOutlet, MatIconModule, LoadingBarModule, LoadingBarHttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
